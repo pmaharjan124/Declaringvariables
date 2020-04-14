8c63af9df520ecc33ff9096c52483e68f8c3adb7
pmaharjan124 <maharjanpuja@gmail.com>
2020-04-14 16:50:44 -0400

    first commit

---
+++ src/Declaringvariables.java
@@ -0,0 +1,16 @@
+public class Declaringvariables {
+    public static void main(String[] args)
+    { String test = "Test Score initialized is :50";
+    System.out.println(test);
+    double price = 24.99;
+    System.out.println(price);
+    String username = "Bobby Johnson";
+        System.out.println(username);
+
+
+
+
+
+
+        }
+    }
